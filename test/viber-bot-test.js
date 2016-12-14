d2b4dec4912556dc13f147b2765b0e96c1502dd7
Idan Harel <idanh@viber.com>
2016-12-14 10:34:41 +0200

    Added missing tests to configuration validation

---
+++ test/viber-bot-test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const TestEnvironmentConfiguration = require(__dirname + "/util/test-environment-configuration");
+const ViberBot = require(__dirname + "/../lib/viber-bot");
+
+exports.creatingBotObject = {
+	createBotWithoutMissingFields: test => {
+		new ViberBot(TestEnvironmentConfiguration.MockLogger, {
+			authToken: "123AB",
+			name: "Test",
+			avatar: "http://avatar.com/image.jpg"
+		});
+		test.done();
+	},
+
+	createBotWithMissingAuthTokenField: test => {
+		test.throws(() => new ViberBot(TestEnvironmentConfiguration.MockLogger, {
+			name: "Test",
+			avatar: "http://avatar.com/image.jpg"
+		}));
+		test.done();
+	},
+
+	createBotWithMissingNameField: test => {
+		test.throws(() => new ViberBot(TestEnvironmentConfiguration.MockLogger, {
+			authToken: "123AB",
+			avatar: "http://avatar.com/image.jpg"
+		}));
+		test.done();
+	},
+
+	createBotWithMissingAvatarField: test => {
+		test.throws(() => new ViberBot(TestEnvironmentConfiguration.MockLogger, {
+			authToken: "123AB",
+			name: "Test",
+		}));
+		test.done();
+	},
+
+	createBotWithoutConfiguration: test => {
+		test.throws(() => new ViberBot(TestEnvironmentConfiguration.MockLogger, {}));
+		test.done();
+	}
+};
